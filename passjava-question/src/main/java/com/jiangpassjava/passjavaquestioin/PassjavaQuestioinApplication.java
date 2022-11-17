4956e4bfb4cbf461665f27a1fe71014a2f1c0a62
jiang <3136573103@qq.com>
2022-11-17 22:31:35 +0800

    第一次提交：完成整体微服务模块的搭建

---
+++ passjava-question/src/main/java/com/jiangpassjava/passjavaquestioin/PassjavaQuestioinApplication.java
@@ -0,0 +1,13 @@
+package com.jiangpassjava.passjavaquestioin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassjavaQuestioinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassjavaQuestioinApplication.class, args);
+    }
+
+}
---
+++ passjava-question/src/test/java/com/jiangpassjava/passjavaquestioin/PassjavaQuestioinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiangpassjava.passjavaquestioin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaQuestioinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
